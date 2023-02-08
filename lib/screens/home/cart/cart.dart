fe40e3005f182daed1488c31ce9cc8da8c1e1425
Zen99 <zen99@pop-os.localdomain>
2023-02-09 00:09:39 +0530

    cart card completed

--- lib/screens/home/cart/cart.dart
+++ lib/screens/home/cart/cart.dart
@@ -22,22 +22,54 @@ class _CartState extends State<Cart> {
           mainAxisAlignment: MainAxisAlignment.center,
           children:[
             Container(
-              width:(MediaQuery.of(context).size.width)/2.5,
+              width:(MediaQuery.of(context).size.width)/3,
               height:(MediaQuery.of(context).size.height),
-              
-              color:Colors.amber
+              margin: EdgeInsets.all(10),
+              child:Container(
+              child: Image.network('https://images.unsplash.com/photo-1511707171634-5f897ff02aa9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bW9iaWxlJTIwcGhvbmV8ZW58MHx8MHx8&w=1000&q=80'),
+              ),
             ),
             SizedBox(width: 4),
             Container(
-              width:(MediaQuery.of(context).size.width)/2,
+              width:(MediaQuery.of(context).size.width)/1.8,
               height:(MediaQuery.of(context).size.height),
-              color:Colors.blue
+              // color:Colors.blue,
+              child: Column(
+                children: [
+                  Container(
+                    width: double.infinity,
+                    margin: EdgeInsets.fromLTRB(10, 6, 10, 4),
+                    child:Text('Samsung A52',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 25)),
+                  ),
+                  Container(
+                    width: double.infinity,
+                    margin: EdgeInsets.fromLTRB(10, 5, 10, 4),
+                    child:Text('by Roshan',style: TextStyle(fontWeight: FontWeight.w400,fontSize: 16,color: Colors.grey)),
+                  ),
+                  Container(
+                    width: double.infinity,
+                    margin: EdgeInsets.fromLTRB(10, 5, 10, 4),
+                    child:Text('120\$',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20)),
+                  ),
+                  
+                  Container(
+                    width: double.infinity,
+                    alignment:  Alignment.topRight,
+                    margin: EdgeInsets.fromLTRB(10, 5, 10, 4),
+                    child:ElevatedButton(
+                    
+                    onPressed: () {},
+                    child: const Text('remove'),
+                  ),
+                  ),
+              ],
+              ),
             ),
           ]  
         ),
       ),
       width:(MediaQuery.of(context).size.width),
-      height:150,
+      height:160,
       
     );
 
@@ -74,7 +106,12 @@ class _CartState extends State<Cart> {
         child:Column(
         children: [
           SizedBox(height:10),
-          cartItem()
+          cartItem(),
+          cartItem(),
+          cartItem(),
+          cartItem(),
+          cartItem(),
+          cartItem(),
         ])
       ),
     );
