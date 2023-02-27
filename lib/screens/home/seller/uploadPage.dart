bc73ba386f5175c1103552ab01d3beff32021e88
Zen99 <roshaaanrulz@gmail.com>
2023-02-27 21:59:07 +0530

    upload page completed

--- lib/screens/home/seller/uploadPage.dart
+++ lib/screens/home/seller/uploadPage.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:dropdown_button2/dropdown_button2.dart';
+import 'package:image_picker/image_picker.dart';
+
 
 class UploadPage extends StatefulWidget {
   const UploadPage({super.key});
@@ -11,6 +13,16 @@ class UploadPage extends StatefulWidget {
 }
 
 class _UploadPageState extends State<UploadPage> {
+  
+  final ImagePicker _imagePicker=ImagePicker();
+
+
+  void selectMultipleImages() async{
+    final XFile? selected = await _imagePicker.pickImage(source:ImageSource.gallery);
+    
+
+  }
+
   final List<String> items = [
   'Item1',
   'Item2',
@@ -21,7 +33,7 @@ String? selectedValue;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-              appBar:AppBar(
+      appBar:AppBar(
         title: const Text('Add Product'),
         backgroundColor: Colors.white,
         foregroundColor: Colors.blue,
@@ -50,6 +62,7 @@ String? selectedValue;
       body:SingleChildScrollView(
         child:Column(children: [
           Container(
+            width: double.infinity,
             padding: EdgeInsets.fromLTRB(35,40,35,15), 
             child: Column(children: [
               Container(
@@ -58,6 +71,7 @@ String? selectedValue;
                 // color: Colors.red,
                 child: Text("Product Name",textAlign: TextAlign.left,style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20))
               ),
+              SizedBox(height: 10),
               TextField(
               decoration: InputDecoration(
                 border: OutlineInputBorder(),
@@ -67,6 +81,7 @@ String? selectedValue;
             ],)
           ),
           Container(
+            width: double.infinity,
             padding: EdgeInsets.fromLTRB(35,40,35,15), 
             child: Column(children: [
               Container(
@@ -75,6 +90,7 @@ String? selectedValue;
                 // color: Colors.red,
                 child: Text("Product Category",textAlign: TextAlign.left,style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20))
               ),
+                SizedBox(height: 10),
                 Container(
                   width: double.maxFinite,
                   // color: Colors.red,
@@ -116,6 +132,7 @@ String? selectedValue;
             ],)
           ),
           Container(
+            width: double.infinity,
             padding: EdgeInsets.fromLTRB(35,40,35,15), 
             child: Column(children: [
               Container(
@@ -124,13 +141,97 @@ String? selectedValue;
                 // color: Colors.red,
                 child: Text("Price",textAlign: TextAlign.left,style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20))
               ),
+              SizedBox(height: 10),
               TextField(
               decoration: InputDecoration(
                 border: OutlineInputBorder(),
                 hintText: 'Enter Price here',
               ),
             ),
+
             ],)
+          ),
+          Container(
+            width: double.infinity,
+            padding: EdgeInsets.fromLTRB(35,40,35,15), 
+            child: Column(children: [
+              Container(
+                width: double.maxFinite,
+                height: 25,
+                // color: Colors.red,
+                child: Text("Add images",textAlign: TextAlign.left,style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20))
+              ),
+              SizedBox(height: 20),
+                Container(
+                  width: double.infinity,
+                  alignment:  Alignment.topLeft,
+                  margin: EdgeInsets.fromLTRB(0, 5, 10, 4),
+                  child:ElevatedButton(
+                  
+                    onPressed: () {
+                      //selectMultipleImages();
+                    },
+                    child: const Text('Upload'),
+                  ),
+                ),
+              // GridView.builder(gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),itemCount: 5, itemBuilder: (BuildContext context,int index){
+              //     return Container(
+              //         alignment: Alignment.center,
+              //         decoration: BoxDecoration(
+              //             color: Colors.amber,
+              //             borderRadius: BorderRadius.circular(15)),
+              //         child: Container(child: Text("hello"),
+              //         ),
+              //     );
+              //   })
+            ],)
+          ),
+          Container(
+            width: double.infinity,
+            padding: EdgeInsets.fromLTRB(35,40,35,15), 
+            child: Column(children: [
+              Container(
+                width: double.maxFinite,
+                height: 25,
+                // color: Colors.red,
+                child: Text("Description",textAlign: TextAlign.left,style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20))
+              ),
+              SizedBox(height: 10),
+              TextField(
+              maxLines: 8,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Description here...',
+              ),
+            ),
+            ],)
+          ),
+          Container(
+            // width: double.infinity,
+            padding: EdgeInsets.fromLTRB(35,40,35,15), 
+            child: Row(children: [
+              Container(
+                alignment:  Alignment.bottomLeft,
+                margin: EdgeInsets.fromLTRB(0, 5, 10, 4),
+                child:ElevatedButton(
+                  onPressed: () {
+                    
+                  },
+                  child: const Text('Save'),
+                ),
+              ),
+              Container(
+                alignment:  Alignment.bottomRight,
+                margin: EdgeInsets.fromLTRB(0, 5, 10, 4),
+                child:ElevatedButton(
+                  
+                  onPressed: () {
+                    
+                  },
+                  child: const Text('Cancel'),
+                ),
+              ),
+            ],),
           ),
         ],)
       ),
